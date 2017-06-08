c631a593e52b46e036481c2ad045be81f5e53986
DansWindowsPC <moran@kgm-associates.com>
2017-06-08 11:16:32 +0200

    initial try to add SVG paths

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ AFRAME.registerComponent('meshline', {
       stringify: function (data) {
         return data.map(AFRAME.utils.coordinates.stringify).join(',');
       }
-    }
+    },
+    // Alternative to 'path', specified as an SVG-stype path string with M,C, H, etc commands. SVG is 2d so Z is always 0.
+    svg: { 
+      type: 'string', 
+      default: ''
+    },
+    curveQuality: { type:'number', default:200}
   },
   
   init: function () {
@@ -69,6 +75,7 @@ AFRAME.registerComponent('meshline', {
   },
   
   update: function () {
+    var data = this.data; // So we can refer to 'data' in closures; since 'this' shifts around
     //cannot use canvas here because it is not created yet at init time
     //console.log("canvas res:");
     //console.log(this.resolution);
@@ -83,13 +90,16 @@ AFRAME.registerComponent('meshline', {
   
     var geometry = new THREE.Geometry();
     
-    this.data.path.forEach(function (vec3) {
-      geometry.vertices.push(
-        new THREE.Vector3(vec3.x, vec3.y, vec3.z)
-      );
-    });
+    if (this.data.svg.length>0 ) {
+    } else {
+      this.data.path.forEach(function (vec3) {
+        geometry.vertices.push(
+          new THREE.Vector3(vec3.x, vec3.y, vec3.z)
+        );
+      });
+    }
     
-    var widthFn = new Function ('p', 'return ' + this.data.lineWidthStyler);
+    var widthFn = function(p){return data.lineWidthStyle; }; // new Function ('p', 'return ' + this.data.lineWidthStyler);
     //? try {var w = widthFn(0);} catch(e) {warn(e);}
     var line = new THREE.MeshLine();
     line.setGeometry( geometry, widthFn );
@@ -100,3 +110,98 @@ AFRAME.registerComponent('meshline', {
     this.el.removeObject3D('mesh');
   }
 });
+
+
+
+
+// Removes commas, adds spaces between commands and coordinates in an SVG string, and split()s it
+function tokenizeSVGPathString(str) {
+  str = str.replace(/\,/g,' ');
+  str = str.replace(/\-/g,' ');
+  str = str.replace(/[A-z]/g,/( $& )/);
+  str = str.replace(/\s+/,' ').trim();
+  return str.split(' ');
+}
+
+// Converts SVG relative coordinates for "m", "c", "h" etc into absolute positions
+function svgRelativeCoordinatesToAbsolute(tok){
+  // We assume the SVG path starts with M x y
+  if (t[0] !== 'M') console.error('Not sure how to handle an SVG path that does not begin with M command');
+  var basisX = t[1];
+  var basisY = t[2];
+  for (var i=3; i<tok.length; i++){
+    if (tok[i].test(/[mlhvcsqtaz]/)) {
+      tok[i]=tok[i].toUpperCase(); // Change this draw command from rel to abs
+      tok[i+1]+= basisX; tok[i+2]+= basisY; // Convert coordinate from rel to abs position
+      basisX=tok[i+1]; basisY=tok[i+2]; // Update the basis for subsequent rel coordinates
+    }
+  }
+  return tok;
+}
+
+
+function svgPathToGeometry(tok, opts){
+    var geometry = new THREE.Geometry();
+    var v = new THREE.Vector3(0,0,0);
+    var c = null;
+
+    for (var i=0; i<tok.length; i++){
+      if (tok[i]=="M"){
+        v = new THREE.Vector3(tok[i+1], tok[i+2], 0);
+        geometry.vertices.push(v.clone());
+        if (i>0) console.warn('SVG path contains M commands after the first command. This is not yet supported and these M commands will be drawn as lines.');
+      }
+      if (tok[i]=="L"){ // L commands can be followed by a number of x,y coordinate pairs
+        for (var j=(i+1); j<tok.length; j+=2){
+          if (!isFiniti(tok[j])) break; 
+          v = new THREE.Vector3(tok[j], tok[j+1], 0);
+          geometry.vertices.push(v.clone());
+        }
+      }
+      if (tok[i]=="H") {
+        v = new THREE.Vector3(tok[i+1], v.y, 0);
+        geometry.vertices.push(v.clone());
+      }
+      if (tok[i]=="V") {
+        v = new THREE.Vector3(v.x, tok[i+1], 0);
+        geometry.vertices.push(v.clone());
+      }
+      if (tok[i]=="A"){
+        console.error("SVG arcs (A) not supported" );
+        // It's not a trivial 1:1 mapping between SVG arcs and Three.EllipseCurve.
+        // The Three object needs to be rotated to use the SVG ellipse rotation, and the SVG object specifies the arc curve
+        // endpoint in x,y coordinates whereas the Three curve wants the start and end in radians
+        /// https://threejs.org/docs/#api/extras/curves/EllipseCurve
+        // https://developer.mozilla.org/en/docs/Web/SVG/Tutorial/Paths#Arcs
+      }
+      if (tok[i]=="C") {
+        c = new THREE.CubicBezierCurve3(
+                    v.clone(),
+                    new THREE.Vector3(tok[i+1], tok[i+2], 0),
+                    new THREE.Vector3(tok[i+3], tok[i+4], 0),
+                    new THREE.Vector3(tok[i+5], tok[i+6], 0)
+                );
+        v = new THREE.Vector3(tok[i+5], tok[i+6], 0); 
+        geometry.vertices.push(c.getSpacedPoints ( opts.curveQuality ));
+      }
+      if (tok[i]=="S" || tok[i]=="T") {
+        console.error('SVG Simplified Beziers (S and T) commands are not currently supported');
+        // Too lazy to implement this at the moment; this is a rare command I think
+        //https://developer.mozilla.org/en/docs/Web/SVG/Tutorial/Paths#Bezier_Curves
+      }
+      if (tok[i]=="Q"){
+        c = new THREE.QuadraticBezierCurve3(
+                    v.clone(),
+                    new THREE.Vector3(tok[i+1], tok[i+2], 0),
+                    new THREE.Vector3(tok[i+3], tok[i+4], 0)
+                );
+        v = new THREE.Vector3(tok[i+3], tok[i+4], 0);
+        geometry.vertices.push(c.getSpacedPoints ( opts.curveQuality ));
+      }
+      if (tok[i]=="Z"){
+        // Draw line to start of path
+        geometry.vertices.push(geometry.vertices[0].clone());
+      }
+    }
+
+}
